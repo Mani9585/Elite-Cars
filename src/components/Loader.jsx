318e99fc0d3a03aeb0609d05fc755b368e98af42
Mani9585 <manikandan98963@gmail.com>
2026-01-01 12:17:11 +0530

    Loading Screen Fix

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
 import "./Loader.css";
 import logo from "../assets/logo.png";
 
 export default function Loader() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress(prev => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          return 100;
+        }
+        return prev + 1;
+      });
+    }, 25);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="loader">
-      <img src={logo} alt="Elite Cars Logo" />
-      <p>ELITE MOTORS</p>
+      <div className="loader-center">
+        <img src={logo} alt="Elite Cars Logo" />
+        <p>ELITE MOTORS</p>
+        <span className="loader-progress">{progress}%</span>
+
+        <div className="loader-bar">
+          <div
+            className="loader-bar-fill"
+            style={{ width: `${progress}%` }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
+
